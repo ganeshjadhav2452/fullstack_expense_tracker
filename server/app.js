f6fdbe600c7f591d9d90d6636497551c8998f345
praful ingale <04praful@gmail.com>
2023-11-09 16:06:23 +0530

    lsdj

--- server/app.js
+++ server/app.js
@@ -16,8 +16,14 @@ const accessLogStream = fs.createWriteStream(
     path.join(__dirname, 'access.log'),
     { flags: 'a' }
 )
+
+const corsOptions = {
+    origin: 'https://start-expense-tracker.onrender.com', // Replace with the domain you want to allow
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Enable cookies and authentication headers
+}
 const app = express()
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(helmet())
 app.use(morgan('combined', { stream: accessLogStream }))
